302e02bb73b26ff85db9651ece84a44b3c332418
gilyeon <12venus08@naver.com>
2022-07-12 05:59:17 +0900

    feat : add event

--- app/03_fastapi_project.py
+++ app/03_fastapi_project.py
@@ -25,3 +25,22 @@ async def root(request: Request):
 async def search(request: Request, q:str):
     print(q)
     return template.TemplateResponse("index.html", {"request": request, "title":'콜렉터 북북이', "keyword":q} )
+
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from odmantic import AIOEngine
+
+from config  import MONGO_DB_NAME,MONGO_URL
+# 참고 https://art049.github.io/odmantic/engine/ engine코드 붙이고, MONGO_URL로 대치
+# 서버 구동될때 DB연결 startup 이벤트 생성
+@app3.on_event("startup")
+async def on_app_start():
+    print("hellooo serverrrrr")
+    client = AsyncIOMotorClient(MONGO_URL)
+    engine = AIOEngine(motor_client=client, database=MONGO_DB_NAME)
+
+
+# 서버꺼지면 구동되는 이벤트
+@app3.on_event("shutdown")
+async def on_app_shutdown():
+    pass
